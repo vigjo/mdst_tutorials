312058caae51f39eb3e7ba2d3f2b52cac77a76ea
Joe Vig <vigjo@umich.edu>
2020-10-07 17:20:11 -0400

    first tutorial

--- tutorial1/python_exercises.py
+++ tutorial1/python_exercises.py
@@ -1,15 +1,27 @@
 """
 Intro to python exercises shell code
 """
+from collections import Counter
 
 def is_odd(x):
-    """
+    if x % 2 != 0:
+	    return false
+    return true
+	"""
     returns True if x is odd and False otherwise
     """
 
+def reverse(s):
+    str = ""
+	for i in s:
+		str = i + str
+	return str
 
 def is_palindrome(word):
-    """
+    if word == word.reverse:
+        return true
+	return false
+	"""
     returns whether `word` is spelled the same forwards and backwards
     """
 
@@ -20,6 +32,11 @@ def only_odds(numlist):
 
     ex: only_odds([1, 2, 3, 4, 5, 6]) -> [1, 3, 5]
     """
+	odds = []
+	for x in numlist:
+	    if is_odd x:
+			odds.append(x)
+	return odds
 
 
 def count_words(text):
@@ -35,3 +52,9 @@ def count_words(text):
         {'how': 1, 'much': 1, 'wood': 1, 'would': 1, 'a': 2, 'woodchuck': 2,
         'chuck': 2, 'if': 1, 'could': 1, 'wood?': 1}
     """
+	text = text.lower()
+	words = text.split(' ')
+	cnt = Counter()
+	for word in text:
+		cnt[word] += 1
+	return cnt
